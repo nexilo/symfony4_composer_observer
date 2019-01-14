53db1fa666c49e11ee085df2e2f77fe81657f06f
Kamil Bednarek <kamil@nexilo.uk>
2019-01-14 17:52:00 +0100

    Change command

--- scripts/command.sh
+++ scripts/command.sh
@@ -3,7 +3,7 @@ until [ -f $COMPOSER_PROJECT_FILE_PATH ]; do
   (>&2 echo "waiting for source...") && ls  -la /source && sleep 2
 done
 
-cd $COMPOSER_PROJECT_PATH && composer install --ignore-platform-reqs && tail -f /dev/null
+cd $COMPOSER_PROJECT_PATH && composer install --ignore-platform-reqs
 
 while inotifywait -q -e modify $COMPOSER_PROJECT_FILE_PATH >/dev/null; do
     cd $COMPOSER_PROJECT_PATH && composer install --ignore-platform-reqs
